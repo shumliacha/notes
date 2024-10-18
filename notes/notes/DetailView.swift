9e9305a9a9b66658c123398aa33d1614f6c258af
shumliacha <126417371+shumliacha@users.noreply.github.com>
2024-10-18 22:41:23 +0200

    Update DetailView.swift

--- notes/notes/DetailView.swift
+++ notes/notes/DetailView.swift
@@ -5,27 +5,46 @@
 //  Created by Kristina Litvinova on 08.10.2024.
 //
 
-//import SwiftUI
-//
-//struct DetailView: View {
-//    
-//    @Binding var note: Note
-//    
-//    var body: some View {
-//        Form {
-//            
-//            Section(header: Text("Title")) {
-//              TextField("Title", text: $note.title ?? "")
-//            }
-//            
-//            Section(header: Text("Note")) {
-//              TextEditor(text: $note.content)
-//            }
-//        }
-//        .navigationTitle("Edit “\(note.title)”")
-//    }
-//}
-//
-//#Preview {
-//    DetailView()
-//}
+import SwiftUI
+
+struct DetailView: View {
+    
+    
+    @State private var title = "Test title"
+    @State private var content = "test content"
+    @State private var datastamp = "11:11 November 11"
+    
+    var body: some View {
+        ZStack{
+            
+            VStack(alignment: HorizontalAlignment.leading){
+                HStack {
+                    Text(title)
+                        .font(.title)
+                        .padding(EdgeInsets(top: 30, leading: 15, bottom: 0, trailing: 15))
+                Spacer()
+                }
+                
+                HStack{
+                    Spacer()
+                    Text(datastamp)
+                        .font(.caption)
+                        .padding(EdgeInsets(top: 0, leading: 15, bottom: 5, trailing: 15))
+                        .foregroundStyle(Color.gray)
+                }
+                
+                Text(content)
+                    .font(.body)
+                    .padding(EdgeInsets(top: 10, leading: 15, bottom: 5, trailing: 15))
+                
+                Spacer()
+            }
+
+        }
+    }
+       
+}
+
+#Preview {
+    DetailView()
+}
